897f89430d3834ee2e105248e2fde4de8a53793f
王笛 Wang,Di <wd1998313@gmail.com>
2016-11-14 19:43:18 -0500

    Initial Commit

---
+++ WhatsRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsRate
+//
+//  Created by Di Wang on 2016/11/14.
+//  Copyright © 2016年 Di Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
